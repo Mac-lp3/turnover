de68e2ea08c6d7191b3bb9705cc6c8ed276504b6
Mac-lp3 <staticlp3@gmail.com>
2016-04-30 19:41:17 -0400

    changed from .xml config to java config

---
+++ src/main/java/org/mac/sim/config/WebInitializer.java
@@ -0,0 +1,22 @@
+package org.mac.sim.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { SpringRootConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { SpringWebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
--- src/main/java/org/mac/sim/controller/SimController.java
+++ src/main/java/org/mac/sim/controller/SimController.java
@@ -2,13 +2,13 @@ package org.mac.sim.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping()
 public class SimController {
-	
-	@RequestMapping(value = "/run", method = RequestMethod.GET)
-	public void runIt(){
+
+	@RequestMapping(path = "/run")
+	public void runIt() {
 		System.out.println("~~~~~~~~~~~~~~");
 	}
 }
