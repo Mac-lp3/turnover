a683fe2ea868cd32d08aedf8c7bca0ac60f0d5ec
Mac-lp3 <staticlp3@gmail.com>
2016-06-08 20:26:20 -0400

    More updates for the Woker loop method.

--- src/main/java/org/mac/sim/thread/Worker.java
+++ src/main/java/org/mac/sim/thread/Worker.java
@@ -3,13 +3,44 @@ package org.mac.sim.thread;
 import java.util.concurrent.BlockingQueue;
 
 public class Worker extends Thread {
-	
+
 	private int periodsToCompleteTask;
 	private int bufferPeriods;
 	private BlockingQueue queue;
+	private boolean loop = true;
 
 	public void run() {
 
+		// get start time / set up
+		long currentPeriod = 0;
+		long nextPeriod = 0;
+		long startTime = System.nanoTime();
+
+		while (loop) {
+
+			try {
+				
+				// check queue. This method will wait if it has to.
+				queue.take();
+				// wait given service time + buffer time.
+				// TODO this is in millis... need to convert somehow
+				this.sleep((periodsToCompleteTask + bufferPeriods) * 1000);
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			// repeat until kill command
+		}
+	}
+
+	public synchronized void doStop() {
+		loop = true;
+		this.interrupt(); // break pool thread out of dequeue() call.
+	}
+
+	public synchronized boolean isStopped() {
+		return loop;
 	}
 
 }
