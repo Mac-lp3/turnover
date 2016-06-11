635bb5d24cc8bc8f53ca9a24a054c536a13ad884
Mac-lp3 <staticlp3@gmail.com>
2016-06-10 20:47:21 -0400

    added more test conditions to measure the time/periods spent in the execution phases

--- src/test/java/org/mac/sim/thread/QueueManagerTest.java
+++ src/test/java/org/mac/sim/thread/QueueManagerTest.java
@@ -15,17 +15,17 @@ public class QueueManagerTest {
 	 */
 	@Test
 	public void runTest() {
-		
+
 		BlockingQueue<WorkerTask> queue = new ArrayBlockingQueue<WorkerTask>(25);
 		QueueManager qm = new QueueManager(queue, 5, 5);
 		qm.run();
 		Assert.assertEquals(25, queue.size());
-		
+
 		queue = new ArrayBlockingQueue<WorkerTask>(50);
 		qm = new QueueManager(queue, 5, 10);
 		qm.run();
 		Assert.assertEquals(50, queue.size());
-		
+
 		queue = new ArrayBlockingQueue<WorkerTask>(500);
 		qm = new QueueManager(queue, 5, 100);
 		qm.run();
@@ -34,20 +34,33 @@ public class QueueManagerTest {
 
 	@Test
 	public void workersTest() {
-		
+
 		BlockingQueue<WorkerTask> queue = new ArrayBlockingQueue<WorkerTask>(500);
 		QueueManager qm = new QueueManager(queue, 5, 100);
-		Worker w = new Worker(queue);
-		
+		Worker w1 = new Worker(queue, 3, 3);
+		Worker w2 = new Worker(queue, 3, 3);
+
 		qm.start();
-		w.start();
-		
+		w1.start();
+		w2.start();
+
+		// few extra nanos saved by declaring variable early (I think)
+		long endTime;
+		long startTime = System.nanoTime();
 		while (!queue.isEmpty()) {
 			System.out.println(queue.size());
 		}
-		
-		w.doStop();
-		
+		endTime = System.nanoTime();
+
+		w1.doStop();
+		w2.doStop();
+
+		System.out.println("Execution time ??? " + Math.floor((endTime - startTime)) / 1000000);
+		System.out.println("Periods in loop: " + qm.getPeriodsSpentInLoop());
+		System.out.println("~~~" + qm.getAddActions());
+		System.out.println("~~~" + w1.getTasksCompleted());
+		System.out.println("~~~" + w2.getTasksCompleted());
+
 	}
 
 }
