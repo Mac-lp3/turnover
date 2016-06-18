29686597d6cdf520f0673620f2a1e882da0cb63e
Mac-lp3 <staticlp3@gmail.com>
2016-06-18 11:54:52 -0400

    Changed tests to use the ClockManager as intended and wait for its run flags to be switched

--- src/test/java/org/mac/sim/thread/QueueManagerTest.java
+++ src/test/java/org/mac/sim/thread/QueueManagerTest.java
@@ -6,6 +6,7 @@ import java.util.concurrent.BlockingQueue;
 import org.junit.Test;
 import org.mac.sim.clock.Clock;
 import org.mac.sim.clock.ClockBuilder;
+import org.mac.sim.clock.ClockManager;
 import org.mac.sim.domain.WorkerTask;
 
 import junit.framework.Assert;
@@ -14,23 +15,51 @@ public class QueueManagerTest {
 
 	/**
 	 * Test that the proper number of tasks have been added to the queue.
+	 * 
+	 * @throws InterruptedException
 	 */
 	@Test
-	public void runTest() {
+	public void runTest() throws InterruptedException {
 
 		BlockingQueue<WorkerTask> queue = new ArrayBlockingQueue<WorkerTask>(25);
-		QueueManager qm = new QueueManager(queue, 5, ClockBuilder.build(5));
-		qm.run();
+
+		Clock fivePeriodClock = ClockBuilder.build(5);
+		ClockManager fiveClockManager = new ClockManager(fivePeriodClock);
+		QueueManager qm = new QueueManager(queue, 5, fivePeriodClock);
+		qm.start();
+		fiveClockManager.start();
+
+		while (!fiveClockManager.isStopped()) {
+
+		}
+
 		Assert.assertEquals(25, queue.size());
 
 		queue = new ArrayBlockingQueue<WorkerTask>(50);
-		qm = new QueueManager(queue, 5, ClockBuilder.build(10));
-		qm.run();
+
+		Clock tenPeriodClock = ClockBuilder.build(10);
+		ClockManager tenClockManager = new ClockManager(tenPeriodClock);
+		QueueManager qm2 = new QueueManager(queue, 5, tenPeriodClock);
+		qm2.start();
+		tenClockManager.start();
+
+		while (!tenClockManager.isStopped()) {
+
+		}
+
 		Assert.assertEquals(50, queue.size());
 
 		queue = new ArrayBlockingQueue<WorkerTask>(500);
-		qm = new QueueManager(queue, 5, ClockBuilder.build(100));
-		qm.run();
+		Clock hundredPeriodClock = ClockBuilder.build(100);
+		ClockManager hundredClockManager = new ClockManager(hundredPeriodClock);
+		qm = new QueueManager(queue, 5, hundredPeriodClock);
+		qm.start();
+		hundredClockManager.start();
+
+		while (!hundredClockManager.isStopped()) {
+
+		}
+
 		Assert.assertEquals(500, queue.size());
 	}
 
@@ -44,16 +73,23 @@ public class QueueManagerTest {
 	public void workersTest() throws InterruptedException {
 
 		BlockingQueue<WorkerTask> queue = new ArrayBlockingQueue<WorkerTask>(500);
-		QueueManager qm = new QueueManager(queue, 5, ClockBuilder.build(100));
+		Clock hundredClock = ClockBuilder.build(100);
+		ClockManager cm = new ClockManager(hundredClock);
+		QueueManager qm = new QueueManager(queue, 5, hundredClock);
 		Worker w1 = new Worker(queue, 3, 3);
 		Worker w2 = new Worker(queue, 3, 3);
 
 		qm.start();
 		w1.start();
 		w2.start();
+		cm.start();
 
-		Thread.sleep(10000);
+		Thread.sleep(200);
+		while (!cm.isStopped()) {
 
+		}
+
+		cm.doStop();
 		w1.doStop();
 		w2.doStop();
 
