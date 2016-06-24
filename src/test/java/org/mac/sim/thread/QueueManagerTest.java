81f0250ba52d5752ca10a4fc2efc1a357c84ef53
Mac-lp3 <staticlp3@gmail.com>
2016-06-24 19:42:03 -0400

    added overloaded workers test to stress the sleep intervals.

--- src/test/java/org/mac/sim/thread/QueueManagerTest.java
+++ src/test/java/org/mac/sim/thread/QueueManagerTest.java
@@ -99,4 +99,55 @@ public class QueueManagerTest {
 
 	}
 
+	/**
+	 * This test should prove that the application is able to adjust the sleep
+	 * intervals appropriately given an excessive amount of workers, task
+	 * length, buffer time, or a combination of all three.
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void overLoadedWorkersTest() throws InterruptedException {
+
+		BlockingQueue<WorkerTask> queue = new ArrayBlockingQueue<WorkerTask>(1000);
+		Clock hundredClock = ClockBuilder.build(100);
+		ClockManager cm = new ClockManager(hundredClock);
+		QueueManager qm = new QueueManager(queue, 10, hundredClock);
+		Worker w1 = new Worker(queue, 5, 5, hundredClock);
+		Worker w2 = new Worker(queue, 5, 5, hundredClock);
+		Worker w3 = new Worker(queue, 5, 5, hundredClock);
+		Worker w4 = new Worker(queue, 5, 5, hundredClock);
+		Worker w5 = new Worker(queue, 5, 5, hundredClock);
+		Worker w6 = new Worker(queue, 5, 5, hundredClock);
+
+		qm.start();
+		w1.start();
+		w2.start();
+		w3.start();
+		w4.start();
+		w5.start();
+		w6.start();
+		cm.start();
+
+		Thread.sleep(200);
+		while (!cm.isStopped()) {
+
+		}
+
+		cm.doStop();
+		w1.doStop();
+		w2.doStop();
+		w3.doStop();
+		w4.doStop();
+		w5.doStop();
+		w6.doStop();
+
+		Assert.assertEquals(1000, qm.getAddActions());
+		int totalTasksCompleted = w1.getTasksCompleted() + w2.getTasksCompleted() + w3.getTasksCompleted()
+				+ w4.getTasksCompleted() + w5.getTasksCompleted() + w6.getTasksCompleted();
+		Assert.assertEquals(1000, totalTasksCompleted);
+		Assert.assertEquals(100, qm.getPeriodsSpentInLoop());
+
+	}
+
 }
