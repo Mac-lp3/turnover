5687813f47c91f7d186702e62c58059a9953cbfb
Mac-lp3 <staticlp3@gmail.com>
2016-06-27 19:11:54 -0400

    removed unneeded url mappings and moved test logic to an actual test class

--- src/main/java/org/mac/sim/controller/SimController.java
+++ src/main/java/org/mac/sim/controller/SimController.java
@@ -1,6 +1,5 @@
 package org.mac.sim.controller;
 
-import org.mac.sim.domain.SimulationBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,27 +9,8 @@ public class SimController {
 
 	@RequestMapping("/")
 	public String index() {
-		System.out.println("plsplsplsplspl");
+
 		return "index";
 	}
 
-	@RequestMapping(path = "/run")
-	public void runIt() {
-
-		SimulationBuilder sb = new SimulationBuilder();
-		sb.setMonthsToPeakProductivity(12);
-		sb.setTotalEmployees(100);
-		sb.setPercentYearlyTurnOver(.06);
-		sb.setYearsToSimulate(2);
-		sb.build();
-
-	}
-
-	@RequestMapping("/thread")
-	public String runThreads() {
-
-		System.out.println("does this work?");
-		return "index.html";
-
-	}
 }
