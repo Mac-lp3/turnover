f3132af4c0a87b6d5e5a0aedb9a546114157eebc
Mac-lp3 <staticlp3@gmail.com>
2016-07-18 15:05:40 -0400

    git status

--- src/main/java/org/mac/sim/controller/ApiController.java
+++ src/main/java/org/mac/sim/controller/ApiController.java
@@ -1,6 +1,9 @@
 package org.mac.sim.controller;
 
 import org.mac.sim.domain.SimulationForm;
+import org.mac.sim.exception.TurnoverException;
+import org.mac.sim.simulation.SimpleQueueSimulationBuilder;
+import org.mac.sim.simulation.Simulation;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,14 +14,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
-	@RequestMapping(path = "/simulation", method = RequestMethod.POST, consumes="application/json;charset=UTF-8")
-	public void postSimulationForm(@RequestParam(value = "type", required = false) String type,
+	@RequestMapping(path = "/simulation", method = RequestMethod.POST, consumes = "application/json;charset=UTF-8")
+	public Simulation postSimulationForm(@RequestParam(value = "type", required = false) String type,
 			@ModelAttribute SimulationForm simulationForm) {
 
-		System.out.println(simulationForm.getQuickStartPeriods());
-		System.out.println(simulationForm.getQuickStartRate());
-		System.out.println(simulationForm.getQuickStartUnits());
-		System.out.println(simulationForm.getQuickStartWorkers());
+		Simulation sim = null;
+
+		// TODO make this a const
+		if ("simple".equalsIgnoreCase(type)) {
+
+			SimpleQueueSimulationBuilder qsb = new SimpleQueueSimulationBuilder(simulationForm.getQuickStartPeriods(),
+					simulationForm.getQuickStartRate());
+
+			qsb.addWorkers(simulationForm.getQuickStartWorkers(), simulationForm.getQuickStartTaskTime(),
+					simulationForm.getQuickStartRestTime());
+
+			try {
+
+				sim = qsb.build();
+
+			} catch (TurnoverException e) {
+
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return sim;
 
 	}
 
