794c097067b989268a9b34e10a855a0261d934b1
Mac-lp3 <staticlp3@gmail.com>
2016-07-28 21:56:31 -0400

    updates to the graph data generation

--- src/main/webapp/app/partials/results/resultsController.js
+++ src/main/webapp/app/partials/results/resultsController.js
@@ -1,25 +1,61 @@
 'use strict';
 
+const $ = require('jquery');
 const Chart = require('chart.js');
 
 module.exports = function ($http, $location, resultsService) {
 
 	const self = this;
 
-	self.init = function () {
+	self.init = function() {
 
-		// mad TODOs here
-		const barChart = new Chart(null, {
-			type: 'bar',
-			data: {
-				labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        		datasets: [{
-            		label: '# of Votes',
-            		data: [12, 19, 3, 5, 2, 3],
-				]}
-			}
+		// Already sorted by period of completion
+		const tasks = resultsService.getSimulationResults().completedTasks;
+		let taskLables = [];
+		let taskWaitTime = [];
+
+		// Build label and wait time list for tasks
+		for (let i = 0; i < tasks.length; ++i) {
+			taskLables.push(tasks[i].arrivalPeriod);
+			taskWaitTime.push(tasks[i].periodsInQueue);
+		}
+
+		const taskBarChartContext = $('#taskBarChart');
+
+		const taskBarChart = new Chart(taskBarChartContext, {
+		 	type: 'bar',
+		 	data: {
+		 		labels: taskLables,
+			 	datasets: [
+			 		{
+			 			data: taskWaitTime
+			 		}
+			 	]
+		 	}
 		});
 
+		const workers = resultsService.getSimulationResults().workers;
+		let workerLables = [];
+		let workerTotals = [];
+
+		for (let i = 0; i < workers.length; ++i) {
+			workerLables.push(i);
+			workerTotals.push(workers[i].tasksCompleted);
+		}
+
+		const workerPieChartContext = $('#workerPieChart');
+
+		const workerPieChart = new Chart(workerPieChartContext, {
+		 	type: 'pie',
+		 	data: {
+		 		labels: workerLables,
+		 		datasets: [
+		 			{
+		 				data: workerTotals
+		 			}
+		 		]
+		 	}
+		 });
 
 	};
 
