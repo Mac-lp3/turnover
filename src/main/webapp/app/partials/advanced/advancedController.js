4be479f0f979189fd5e67a2b4b76a8d8be9a8eaf
Mac-lp3 <staticlp3@gmail.com>
2016-08-02 11:08:44 -0400

    added functions to toggle active configuration

--- src/main/webapp/app/partials/advanced/advancedController.js
+++ src/main/webapp/app/partials/advanced/advancedController.js
@@ -5,5 +5,17 @@ module.exports = function ($http, $location, resultsService) {
 	const self = this;
 	self.periodUnits = 'minutes';
 	self.totalPeriods = 0;
+	self.isProbability = false;
+	self.isLinear = false;
+
+	self.showLinearConfig = () => {
+		self.isProbability = false;
+		self.isLinear = true;
+	};
+
+	self.showProbabilityConfig = () => {
+		self.isProbability = true;
+		self.isLinear = false;
+	};
 
 };
