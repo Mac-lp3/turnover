98d7ba91536a1feeff36d31764e75648c7d83b64
Mac-lp3 <staticlp3@gmail.com>
2016-08-02 22:21:34 -0400

    added initialization logic for probability config

--- src/main/webapp/app/partials/advanced/advancedController.js
+++ src/main/webapp/app/partials/advanced/advancedController.js
@@ -8,12 +8,34 @@ module.exports = function ($http, $location, resultsService) {
 	self.isProbability = false;
 	self.isLinear = true; // default
 
-	self.showLinearConfig = () => {
+	// linear configuration
+	self.linearTaskConfigs = [];
+
+	// initialize with one value
+	self.linearTaskConfigs.push({
+		arrivalRate: 0,
+		taskLength: 0,
+		startPeriod: 0
+	});
+
+	// probability configuration
+	self.probabilityTaskConfigs = [];
+
+	// initialize with one value
+	self.probabilityTaskConfigs.push({
+		arrivalRate: 0,
+		lowBound: 0,
+		highBound: 0,
+		taskProportion: 100,
+		startPeriod: 0
+	});
+
+	self.showLinearConfig = function () {
 		self.isProbability = false;
 		self.isLinear = true;
 	};
 
-	self.showProbabilityConfig = () => {
+	self.showProbabilityConfig = function () {
 		self.isProbability = true;
 		self.isLinear = false;
 	};
