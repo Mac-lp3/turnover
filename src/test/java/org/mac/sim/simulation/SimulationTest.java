f5fc176e029e5d7414bb88a3e35729cb31df1687
Mac-lp3 <staticlp3@gmail.com>
2016-08-17 22:53:19 -0400

    added test for linear simulation

--- src/test/java/org/mac/sim/simulation/SimulationTest.java
+++ src/test/java/org/mac/sim/simulation/SimulationTest.java
@@ -36,25 +36,21 @@ public class SimulationTest {
 
 	@Test
 	public void simpleQueueSimulationTest() throws TurnoverException {
-		
+
 		SimulationConfigrations simpleSimulationConfiguration = new SimulationConfigrations();
-		
+
 		// set clock details
 		simpleSimulationConfiguration.setTotalPeriods(100);
 		simpleSimulationConfiguration.setClockUnits("Minutes");
-		
+
 		// set task config
 		TaskConfiguration taskConfig = new TaskConfiguration();
 		taskConfig.setRate(8);
 		taskConfig.setStartPeriod(0);
-		
+
 		List<TaskConfiguration> taskConfigList = new ArrayList<TaskConfiguration>();
 		taskConfigList.add(taskConfig);
 		simpleSimulationConfiguration.setTaskConfigurations(taskConfigList);
-		
-		WorkerConfiguration workerConfig1 = new WorkerConfiguration();
-		WorkerConfiguration workerConfig2 = new WorkerConfiguration();
-		WorkerConfiguration workerConfig3 = new WorkerConfiguration();
 
 		SimpleQueueSimulationBuilder qsb = new SimpleQueueSimulationBuilder(simpleSimulationConfiguration);
 		qsb.addWorkers(5, 10, 5);
@@ -72,5 +68,60 @@ public class SimulationTest {
 		for (Worker worker : sim.getWorkers()) {
 			System.out.println(worker.getTasksCompleted());
 		}
+	}
+
+	@Test
+	public void linearSimTest() throws TurnoverException {
+
+		WorkerConfiguration workerConfig1 = new WorkerConfiguration();
+		workerConfig1.setArrivalPeriod(0);
+		workerConfig1.setRestTime(1);
+		workerConfig1.setStopPeriod(33);
+		workerConfig1.setAdditionalTime(0);
+		workerConfig1.setTotal(4);
+		
+		LinearQueueSimulationBuilder builder = new LinearQueueSimulationBuilder(100, "Minutes");
+		builder.addWorkerConfig(workerConfig1);
+
+		WorkerConfiguration workerConfig2 = new WorkerConfiguration();
+		workerConfig2.setAdditionalTime(1);
+		workerConfig2.setArrivalPeriod(33);
+		workerConfig2.setRestTime(2);
+		workerConfig2.setStopPeriod(66);
+		workerConfig2.setTotal(5);
+		builder.addWorkerConfig(workerConfig2);
+
+		WorkerConfiguration workerConfig3 = new WorkerConfiguration();
+		workerConfig3.setAdditionalTime(-1);
+		workerConfig3.setArrivalPeriod(66);
+		workerConfig3.setRestTime(1);
+		workerConfig3.setStopPeriod(100);
+		workerConfig2.setTotal(3);
+		builder.addWorkerConfig(workerConfig3);
+
+		TaskConfiguration taskConf1 = new TaskConfiguration();
+		taskConf1.setEndPeriod(25);
+		taskConf1.setLength(4);
+		taskConf1.setRate(4);
+		taskConf1.setStartPeriod(0);
+		builder.addTaskConfig(taskConf1);
+		
+		TaskConfiguration taskConf2 = new TaskConfiguration();
+		taskConf2.setEndPeriod(66);
+		taskConf2.setLength(3);
+		taskConf2.setRate(5);
+		taskConf2.setStartPeriod(25);
+		builder.addTaskConfig(taskConf2);
+		
+		TaskConfiguration taskConf3 = new TaskConfiguration();
+		taskConf3.setEndPeriod(100);
+		taskConf3.setLength(5);
+		taskConf3.setRate(5);
+		taskConf3.setStartPeriod(66);
+		builder.addTaskConfig(taskConf3);
+		
+		Simulation results = builder.build();
+		results.getCompletedTasks();
+
 	}
 }
