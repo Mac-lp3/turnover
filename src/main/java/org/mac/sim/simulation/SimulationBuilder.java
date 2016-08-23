5a903cdf0e75732a601109b3e87d5e9fcc0f4262
Mac-lp3 <staticlp3@gmail.com>
2016-08-22 22:44:34 -0400

    java doc comments

--- src/main/java/org/mac/sim/simulation/SimulationBuilder.java
+++ src/main/java/org/mac/sim/simulation/SimulationBuilder.java
@@ -2,13 +2,25 @@ package org.mac.sim.simulation;
 
 import org.mac.sim.domain.SimulationConfigurations;
 import org.mac.sim.domain.SimulationParameters;
+import org.mac.sim.exception.TurnoverException;
 
+/**
+ * SimulationBuilder implementations are meant to 1) encapsulate the logic which
+ * extracts relevant information from the configuration, 2) provide simple
+ * methods for modifying/editing this information, all while 3) hiding the
+ * details of the Simulation implementation itself.
+ * 
+ * @author Home
+ *
+ */
 public abstract class SimulationBuilder {
 	
 	protected SimulationParameters simulationParams;
 	
 	protected SimulationBuilder(SimulationConfigurations simulationConfig) {
-		
+
 	}
-	
+
+	public abstract Simulation build() throws TurnoverException;
+
 }
