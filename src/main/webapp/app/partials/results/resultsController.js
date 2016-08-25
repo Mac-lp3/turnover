b8d1a967e031dca3a8750edc6912b69292a7a947
Mac-lp3 <staticlp3@gmail.com>
2016-08-24 20:03:58 -0400

    removed declaration and references to self

--- src/main/webapp/app/partials/results/resultsController.js
+++ src/main/webapp/app/partials/results/resultsController.js
@@ -5,31 +5,36 @@ const Chart = require('chart.js');
 
 module.exports = function ($http, $location, resultsService) {
 
-	const self = this;
-	self.averageWaitTime = 0;
-	self.averageTasksPerWorker = 0;
-	self.colorMap = {};
+	this.averageWaitTime = 0;
+	this.averageTasksPerWorker = 0;
+	this.colorMap = {};
+
+	this.advancedConfig = () => {
+
+        $location.path('/advanced');
+
+    };
 
 	/*
 	 * Get unique grayscale for given seed
 	 */
-	self.getGrayScale = (seed) => {
+	this.getGrayScale = (seed) => {
 
 		// if a color has not been generated for this seed, do so
-		if (self.colorMap[seed] == null) {
+		if (this.colorMap[seed] == null) {
 			
 			const value = Math.random() * 0xFF | 0;
 			const grayscale = (value << 16) | (value << 8) | value;
 			const color = '#' + grayscale.toString(16);
 			
-			self.colorMap[seed] = color;
+			this.colorMap[seed] = color;
 		}
 
 		// return the color
-		return self.colorMap[seed];
+		return this.colorMap[seed];
 	};
 
-	self.buildChartData = () => {
+	this.buildChartData = () => {
 
 		// Already sorted by period of completion
 		const tasks = resultsService.getSimulationResults().completedTasks;
@@ -42,11 +47,11 @@ module.exports = function ($http, $location, resultsService) {
 		for (let i = 0; i < tasks.length; ++i) {
 			taskLables.push(tasks[i].arrivalPeriod);
 			taskWaitTime.push(tasks[i].periodsInQueue);
-			taskBarColors.push(self.getGrayScale(tasks[i].configHashCode));
+			taskBarColors.push(this.getGrayScale(tasks[i].configHashCode));
 			totalWait += tasks[i].periodsInQueue;
 		}
 
-		self.averageWaitTime = totalWait / tasks.length;
+		this.averageWaitTime = totalWait / tasks.length;
 
 		const taskBarChart = new Chart($('#taskBarChart'), {
 		 	type: 'bar',
@@ -86,12 +91,12 @@ module.exports = function ($http, $location, resultsService) {
 		for (let i = 0; i < workers.length; ++i) {
 			workerLables.push('Worker ' + (i + 1));
 			workerTotals.push(workers[i].tasksCompleted);
-			workerColors.push(self.getGrayScale(workers[i].periodsToCompleteTask));
+			workerColors.push(this.getGrayScale(workers[i].periodsToCompleteTask));
 			totalTasksCompleted += workers[i].tasksCompleted;
 		}
 
 		// calculate average
-		self.averageTasksPerWorker = totalTasksCompleted / workers.length
+		this.averageTasksPerWorker = totalTasksCompleted / workers.length
 
 		const workerPieChart = new Chart($('#workerPieChart'), {
 		 	type: 'pie',
@@ -138,6 +143,6 @@ module.exports = function ($http, $location, resultsService) {
 
 	};
 
-	self.buildChartData();
+	this.buildChartData();
 
 };
